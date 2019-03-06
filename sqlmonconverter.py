a2842332abce75b9e02f75bea43031e64898370c
Guilherme Augusto Maas <guilherme.maas@gmail.com>
2019-03-06 08:03:54 -0300

    Update sqlmonconverter.py
    .

--- sqlmonconverter.py
+++ sqlmonconverter.py
@@ -1,12 +1,45 @@
 f = open('C:\\Users\\guilherme.maas\\Documents\\dev\\sqlmon-converter\\sqlmonexemplo.txt', 'r')
 
 dict = {}
-word = 'DATA IN'
-wordcount = 0
+update_count = 0
+insert_count = 0
+select_count = 0
+delete_count = 0
+palavaras_count = 0
 
 for words in f.read().split():
-    if word in words:
-        wordcount =+ 1
+    print(f'PALAVRA NÚMERO: {palavaras_count}')
+    print(words)
 
-print(f.read())
+    if 'UPDATE' in words:
+        update_count +=1
+    
+    if 'INSERT' in words:
+        insert_count += 1
+
+    if 'DELETE' in words:
+        delete_count += 1
+    
+    if 'SELECT' in words:
+        select_count += 1
+
+    palavaras_count += 1
+
+print(f'\nForam realizados o total de {update_count} UPDATES neste log.')
+print(f'\nForam realizados o total de {delete_count} DELETES neste log.')
+print(f'\nForam realizados o total de {insert_count} INSERTS neste log.')
+print(f'\nForam realizados o total de {select_count} SELECTS neste log.')
+
+update_dict = {}
+
+with open('C:\\Users\\guilherme.maas\\Documents\\dev\\sqlmon-converter\\sqlmonexemplo.txt', 'r') as f:
+    for line in f:
+        if 'UPDATE' in line:
+            split_line = line.split()
+            update_dict[split_line[0]] = split_line[1]
+        else:
+            pass
+
+print(update_dict)
+#print(f.read())
 print(type(f))
