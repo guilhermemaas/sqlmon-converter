2fb7bbb4866e7545ea24ad933908fd013b121702
Guilherme Augusto Maas <guilherme.maas@gmail.com>
2019-03-15 02:46:24 -0300

    Update sqlmontosqlregex.py
    .

--- sqlmontosqlregex.py
+++ sqlmontosqlregex.py
@@ -30,13 +30,13 @@ def split_lines(block_id):
     """
     line_dict = {}
     line_list = []
-    with open('C:\\Users\\guilherme.maas\\Documents\\dev\\sqlmon-converter\\sqlmonexemplo.txt', 'r') as text_file:
+    with open('C:\\Users\sword art\\Documents\\dev\\sqlmon-converter\\sqlmonexemplo.txt', 'r') as text_file:
         for line in text_file:
             split_line = line.split()
 
             regex_exec = re.search(r'\d{2}\:\d{2}\:\d{2}\s{2}(.*)\s{1}\-', line)
             print(f'tipo_comando regex: {regex_exec.group(1)}')
-            line_dict['type_line'] = define_type(regex_exec)
+            line_dict['type_line'] = define_type(regex_exec.group(1))
 
             regex_exec = re.search(r'(.*)\s{2}\d{2}\:', line)
             print(f'linha regex: {regex_exec.group(1)}')
@@ -53,6 +53,15 @@ def split_lines(block_id):
             comando = regex_exec.group(1)
             line_dict['command'] = comando
 
+            #regex_exec = re.search(r'(\d*)\s{2}\d{2}\:\d{2}\:\d{2}', line)
+            regex_exec = re.search(r'(.*)\s{2}(\d{2}\:\d{2}\:\d{2})\s{2}(.*)\s{1}\-{1}\s{1}(.*)', line)
+            regex_full_linha = regex_exec.group(1)
+            regex_full_hora = regex_exec.group(2)
+            regex_full_tipo = regex_exec.group(3)
+            regex_full_comando = regex_exec.group(4)
+            print('*'*20)
+            print(f'Regex FULL\nLinha: {regex_full_linha} | Hora: {regex_full_hora} | Tipo: {regex_full_tipo} | Comando: {regex_full_comando}\n' + '*' * 20)
+            
             print(block_id)
             line_dict['block'] = block_id
             block_id += 1
