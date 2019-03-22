99defb62777b9dd3132b1a607022c61921b51e71
Guilherme Augusto Maas <guilherme.maas@gmail.com>
2019-03-22 08:02:34 -0300

    .
    .

---
+++ interfacetk.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+class Application:
+    def __init__(self, master=None):
+        self.widget1 = Frame(master)
+        self.widget1.pack()
+        self.msg = Label(self.widget1, text="Teste")
+        self.msg.pack()
+root = Tk()
+Application(root)
+root.mainloop()
